670ada98582e52fed5da7ccc2135b82e8397e1fd
unknown <felixcodesg@gmail.com>
2019-09-14 00:22:48 -0430

    added theme provider from styled components

--- client/src/index.js
+++ client/src/index.js
@@ -1,33 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // redux and react redux
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import rootReducer from './reducers'
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-// react router dom
-import { BrowserRouter as Router, } from 'react-router-dom' 
+// Styled components theme
+import { ThemeProvider } from 'styled-components';
+import theme from './utils/theme';
+import GlobalStyles from './utils/globals';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootswatch/dist/darkly/bootstrap.min.css';
 
 // put any middleware inside the array
 const middleWare = [thunk];
 
 // my store
-const store = createStore(rootReducer, compose(
-  applyMiddleware(...middleWare),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(...middleWare),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router>
-      <App /> 
-    </Router>
-  </Provider>, document.getElementById('root'));
-
-serviceWorker.unregister();
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <Router>
+        <App />
+        <GlobalStyles />
+      </Router>
+    </Provider>
+  </ThemeProvider>,
+  document.getElementById('root')
+);
