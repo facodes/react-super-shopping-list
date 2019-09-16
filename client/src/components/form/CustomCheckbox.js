12353533d9083c46f7f78e16e3608f285b4eb5e1
unknown <felixcodesg@gmail.com>
2019-09-16 17:41:16 -0430

    using a fa icon for check

--- client/src/components/form/CustomCheckbox.js
+++ client/src/components/form/CustomCheckbox.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const ChechboxLabelWrapper = styled.label``;
 
@@ -8,12 +9,11 @@ const CheckboxContainer = styled.div`
   vertical-align: middle;
 `;
 
-const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
-  // Hide checkbox visually but remain accessible to screen readers.
-  // Source: https://polished.js.org/docs/#hidevisually
+// Hide checkbox visually but remain accessible to screen readers.
+// Source: https://polished.js.org/docs/#hidevisually
+const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`  
   border: 0;
   clip: rect(0 0 0 0);
-  /* clippath: inset(50 %); */
   height: 1px;
   margin: -1px;
   overflow: hidden;
@@ -27,24 +27,25 @@ const Label = styled.label`
   font-size: 1.8rem;
   margin-left: 8px;
   pointer-events: none;
+  display: inline-block;
+  vertical-align: middle;
 `;
 
 const Icon = styled.svg`
-  fill: none;
-  stroke: white;
-  stroke-width: 2px;
+   width:80%;
+   height:80%;
+   transition: all 0.15s ease-in-out;
 `;
 
 const StyledCheckbox = styled.div`
-  display: inline-block;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 2.8rem;
   height: 2.8rem;
-  background: pink;
-
-  transition: all 150ms;
+  
 
   /* gradient radius trick */
-
   border: double 3px transparent;
   border-radius: 8px;
   background-image: linear-gradient(var(--color-dark), var(--color-dark)),
@@ -55,9 +56,10 @@ const StyledCheckbox = styled.div`
     );
   background-origin: border-box;
   background-clip: content-box, border-box;
+  /* End of trick */
 
   ${Icon} {
-    visibility: ${props => (props.checked ? 'visible' : 'hidden')};
+    opacity: ${props => props.checked ? 1 : 0};
   }
 `;
 
@@ -67,8 +69,8 @@ function CustomCheckbox({ label, checked, ...props }) {
       <CheckboxContainer>
         <HiddenCheckbox checked={checked} {...props} />
         <StyledCheckbox checked={checked}>
-          <Icon viewBox="0 0 24 24">
-            <polyline points="20 6 9 17 4 12" />
+          <Icon>
+            <FontAwesomeIcon icon="check" />
           </Icon>
         </StyledCheckbox>
       </CheckboxContainer>
