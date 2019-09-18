4be55776e2526da3b2376033d5cae29e124de6de
felix <felixcodesg@gmail.com>
2019-09-18 18:09:16 -0400

    added an styled button and export it

--- client/src/components/Buttons.js
+++ client/src/components/Buttons.js
@@ -2,20 +2,27 @@ import React from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export const Button = styled.button`
+const StyledButton = styled.button`
   color: ${ props =>
     props.color !== 'light' ? 'var(--color-ligth) ' : 'var(--color-dark)'
   };
   background: ${ props =>
     props.color ? `var(--color-${props.color})` : `var(--color-primary)`
   };
+  
   border: none;
   outline: none;
   border-radius: 64px;
   line-height: 1;
   font-weight: var(--bold);
   text-transform: uppercase;
-  padding: ${ ({size}) => {
+  min-width: ${({size}) => {
+  	if (size == 'sm')
+  		return 0
+  	else
+  		return `14rem`
+  }};
+  padding: ${({size}) => {
     if (size === 'lg')
       return // this is not set
     else if(size === 'md')
@@ -23,11 +30,17 @@ export const Button = styled.button`
     else if ( size === 'sm')
       return `0.4em 1em`
     else 
-      return `0.4em 2em`
+      return `0.4em 1em`
   }};
   font-size: ${props => (props.fontSize ? `${props.fontSize}rem` : `2rem`)};
 `;
 
+export const Button = ({...props}) => {
+	return <StyledButton {...props}>{props.children}</StyledButton>
+}
+
+
+// Icon Buttons
 const StyledIconButton = styled.button`
   color: var(--color-light);
   border:0;
