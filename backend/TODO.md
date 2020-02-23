97a56f82bed33a6b9084dcfb3c528395a9e68372
felixlopz <felixlopzd@gmail.com>
2020-02-23 00:17:48 -0400

    cleaning the backend

--- backend/TODO.md
+++
@@ -1,6 +0,0 @@
-- [x] Add to shopping list model attribute budget
-- [x] Delete to Shopping List model attribute done
-- [x] Add to item model price and quantity
-- [x] Create update route for shopping list and items 
-- [x] user model: email name lastname password
-- [x] update login to do it with email
--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,6 @@ mongoose
   .catch((err)=> console.log(err));
 
 // routes
-// app.use ('/api/items' , require('./routes/api/items'));
 app.use ('/api/signin' , require('./routes/api/signin'));
 app.use ('/api/login' , require('./routes/api/login'));
 app.use ('/api/user' , require('./routes/api/user'));
@@ -31,16 +30,6 @@ app.use ('/api/user' , require('./routes/api/user'));
 app.get("/", (req, res) => {
   res.send("super shopping list API");
 });
-
-
-/* Production */
-// if(process.env.NODE_ENV === 'production'){
-//   // Set static folder
-//   app.use(express.static('client/build'));
-//   app.get('*' , (req, res) => {
-//     res.sendFile(path.resolve(__dirname,'client', 'build' , 'index.html'))
-//   })
-// }
 
 const port = process.env.PORT || 5000;
 app.listen(port, ()=>{
