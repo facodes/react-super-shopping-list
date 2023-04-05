99e05c0459de96956ec751b852fdde62253cb357
Felix Lopez <felixlopzd@gmail.com>
2023-04-04 22:23:01 -0400

    fixing merge conflicts from pull from master

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const serverless = require('serverless-http');	
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv/config');
-
+const express = require("express");
+const serverless = require("serverless-http");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
 
 const app = express();
 
 // middlewares
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,29 +18,43 @@ app.use(cookieParser());
 
 // Connecting to mongo
 mongoose
-  .connect(process.env.MONGO_CONNECTION, {useNewUrlParser: true , useCreateIndex: true, useUnifiedTopology: true})
-  .then(()=> console.log('MongoDB Connected'))
-  .catch((err)=> console.log(err));
+  .connect(process.env.MONGO_CONNECTION, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+    initializeRoutes();
+  })
+  .catch((err) => console.log(err));
 
-const netlifyRoot = '/.netlify/functions';
+function initializeRoutes() {
+  const netlifyRoot = "/.netlify/functions";
 
-// routes
-app.use (`/api/signin` , require('./routes/api/signin'));
-app.use (`/api/login` , require('./routes/api/login'));
-app.use (`/api/user` , require('./routes/api/user'));
+  const apiRoot = process.env.NODE_ENV === "production" ? netlifyRoot : "";
 
+  // routes
+  app.use(`${apiRoot}/api/signin`, require("./routes/api/signin"));
+  app.use(`${apiRoot}/api/login`, require("./routes/api/login"));
+  app.use(`${apiRoot}/api/user`, require("./routes/api/user"));
 
-// Basic get rout
-const router = express.Router(); 
+  // Basic get rout
+  const router = express.Router();
 
-app.get('/', (req, res) => {
-	res.send("Super shopping list API see https://github.com/felixlopz/react-super-shopping-list for more info");
-});
+  router.get("/", (req, res) => {
+    res.send(
+      `Super shopping list API see ${process.env.REPOSITORY_URL} for more info`
+    );
+  });
 
-// app.use (`${netlifyRoot}/api/`,  router )
+  app.use(`${apiRoot}/api/`, router);
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000/');
-})
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`API server listening on port http://localhost:${port}`);
+  });
+}
 
-
+module.exports = app;
+module.exports.handler = serverless(app);
