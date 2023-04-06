e40a9164138a3009f8dcd3a8489cc947cc6dfbf5
Felix Lopez <felixlopzd@gmail.com>
2023-04-05 21:39:44 -0400

    adding other changes

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static("public"));
 
 // Connecting to mongo
 mongoose
@@ -31,20 +32,24 @@ mongoose
 const apiRoot = "/api";
 
 // routes
-app.use(`${apiRoot}/signin`, require("../routes/api/signin"));
-app.use(`${apiRoot}/login`, require("../routes/api/login"));
-app.use(`${apiRoot}/user`, require("../routes/api/user"));
+app.use(`${apiRoot}/signin`, require("./src/routes/api/signin"));
+app.use(`${apiRoot}/login`, require("./src/routes/api/login"));
+app.use(`${apiRoot}/user`, require("./src/routes/api/user"));
 
 // Basic get rout
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send(
-    `Super shopping list API see ${process.env.PROJECT_REPOSITORY_URL} for more info`
-  );
+// router.get("/", (req, res) => {
+//   res.send(
+//     `Super shopping list API see ${process.env.PROJECT_REPOSITORY_URL} for more info`
+//   );
+// });
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
 });
 
-app.use(`${apiRoot}/`, router);
+// app.use(`${apiRoot}/`, router);
 
 module.exports = app;
 // module.exports.handler = serverless(app);
